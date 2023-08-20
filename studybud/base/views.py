961fa24de22c669d820b80ea6c2f1e23988aa08d
Hardikmaind <maindhardik@gmail.com>
2023-08-20 17:28:27 +0530

    created a context dictnary

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -4,8 +4,14 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 
 # now to call the home function, we need to import it into the urls.py file
+rooms=[
+    {'id':1,'name':'Lets Learn Python'},
+    {'id':2,'name':'Lets play Django'},
+    {'id':3,'name':'lets play guitar'},
+]
 
 def home(request):
-    return render(request,'home.html')
+    context={'rooms':rooms} #this is a context dictionary
+    return render(request,'home.html',context)
 def room(request):
     return render(request,'room.html')
