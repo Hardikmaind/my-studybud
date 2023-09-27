4da72ee35b832809cc50393527363e8c9d695cd8
Hardikmaind <maindhardik@gmail.com>
2023-09-27 11:36:52 +0530

    removed all the comments of before code

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,80 +1,47 @@
 from django.db import models
-from django.contrib.auth.models import User     #here we are importing the user model from django.contrib.auth.models...later on we will customize it
+
+from django.contrib.auth.models import User
 
 
-# here all the tables are created
-# Create your models here.
-
-
-
-# Room is gonna be tha child of a topic...  i wanna specify a class above it..that is gonna be the parent class of this class
 class Topic(models.Model):
-    name=models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+
     def __str__(self):
         return self.name
-        # now we wanna speciy the relation ship between the room and the topic...a topic can have multile rooms whereas a room can have only one topic
-
-
-
 
 
 class Room(models.Model):
-    name=models.CharField( max_length=200)  #here null is set to false by default. this means is ..the database cannot have the instance of this model without having this value something in it
-    description=models.TextField(null=True ,blank=True)      #edar bydefault null ki value false rehti isliye aoan ne specify kiya hain ki null ki value true rahe. here it means that null is allowed
-    #here blank true means hum jab form banate tab hum blank  use send kar sakte. when we run the save method,like we submit a form..that form  can also be empty.Here it makes sure that this description value cannot be left blank
 
-    #partiipants=
-    updated=models.DateTimeField(auto_now=True)   #this is gonna take the snapshot of any time,this model instace was updated,so any time we run teh save mehod to update this model or this specifi table or item...this is going to take a time stamp. here we want the date and the time field it happend. here auto_now =true means that every time the save method is called this is going to take tha sanpshot automatically  
+    name = models.CharField(max_length=200)
 
-    created=models.DateTimeField(auto_now_add=True)
-    #diff between autonow and autonnowadd is auto noew add only takes snapshot when we first create or save this instant..if we save the instance multiple time ..this value will never change ...where as autonow will take snapchot every single time 
+    description = models.TextField(null=True, blank=True)
 
-    #  dafault we have ids genrated for them from 1
-    # id= we can override  the id field by creating our own id field with 
+    updated = models.DateTimeField(auto_now=True)
 
+    created = models.DateTimeField(auto_now_add=True)
 
-    # since topic class is placed below Room class in our CODE  we cann access it by putting like 'Topic'.(in quotes)..but if it would have been above  then we can aslo access it by putting just Topic
-    topic=models.ForeignKey(Topic,null=True ,on_delete=models.SET_NULL)       #this is a one to many relationship...and in bracket there is the model we are relating to. i.e parent model topic . incase topic is deleted then the room will be set to null, idont want to delete the room..i just want to set it to null. and if this value is set to null then we have to allow in the database that this value can be null. so we have to specify null=True
+    topic = models.ForeignKey(Topic, null=True, on_delete=models.SET_NULL)
 
-    
-    host=models.ForeignKey(User,null=True ,on_delete=models.SET_NULL)
+    host = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
-
-
-    # The __str__ and __repr__ methods are two commonly used “dunder” (double underscore) methods that can be used to define how an object is represented as a string. Both methods, str and repr, generate a string representation of an object, but they serve different purposes and people use them in different contexts.2
-
-    # When you call str(object) on an instance of your class, Python will look for the __str__ method in that class and use its return value as the string representation of the object.
-
-    # when i create a new room...all the room will be sorted by the time they were updaterd and created.because of this below meta class
     class Meta:
-        # ordering=['updated','created']  
-            #its gonna order the room by the time they were updated and created in ascending order. if we want to order in descending order then we can do like this ordering=['-updated','-created']
-        ordering=['-updated','-created']    #in this the newest  updateed room object(row in table Room) will be first and the oldest will be last
 
+        ordering = ['-updated', '-created']
 
-
-    # now when i create a new room object..i want to see the name of the room in the admin panel..so i have to create a string representation of the room object
-    def __str__(self):          #created a string representation of our room
+    def __str__(self):
         return self.name
-        # return str(self.name)       #here self has to be string ...if not then we have to type cast it
 
 
-#after we add a new model...we have to again do the migration with the command "python manage.py makemigrations"  then press enter. after this then i hace to do "python manage.py migrate"..before this command it is like a staging area...we have to this everytime. after apply both the commands. this will go into the latest migrations .after applyig this migrations theis migrations are in the database
+class Message(models.Model):
 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
 
-class Message(models.Model):  
-    # django already builds the usermodel for us..we can use that or we can create our own user model  
-    user=models.ForeignKey(User,on_delete=models.CASCADE)       #this is a one to many relationship...and in bracket there is the model we are relating to. i.e parent model user   
-    room =models.ForeignKey(Room,on_delete=models.CASCADE)       #this is a one to many relationship...and in bracket there is the model we are relating to. i.e parent model room
-    # on_delete=models.SET_NULL  means that if we delete the room..then the message will not be deleted..it will be set to null. all the instance of the children will be set to null  
-    # on_delete=models.CASCADE  means that if we delete the room..then the message will also be deleted..it will be set to null. all the instance of the children will be set to null
-    body=models.TextField()
-    updated=models.DateTimeField(auto_now=True) 
-    created=models.DateTimeField(auto_now_add=True) 
-     
-    def __str__(self):          #created a string representation of our room
-        return self.body[0:50 ]  #here we are returning the first 50 characters of the body of the message
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
 
-
+        return self.body[0:50]
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,227 +1,124 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.db.models import Q
-from .forms import RoomForm 
+from .forms import RoomForm
 from django.http import HttpResponse
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm          #we will customise tis form later ..but for now ets use this default
+from django.contrib.auth.forms import UserCreationForm
 from django.http.response import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Room ,Topic     #first we import the model that we want to query
-
-# Create your views here.
-
-
-# now to call the home function, we need to import it into the urls.py file
-# rooms=[
-#     {'id':1,'name':'Lets Learn Python'},
-#     {'id':2,'name':'Lets play Django'},
-#     {'id':3,'name':'lets play guitar'},
-# ]
-
-
-# Q() is used to create complex queries. It allows you to use | (OR) and & (AND) operators to combine conditions
-# topic__name__icontains=q is a lookup expression. It means you're querying the Room model for objects where the related Topic's name contains (case-insensitive) the value of q.
-# name__icontains=q is querying the Room model for objects where the name field contains (case-insensitive) the value of q.
-# In simpler terms, this query is looking for rooms where either the related Topic's name contains the value of q or the room's own name field contains the value of q. The icontains is for case-insensitive matching.
-
-# The double underscores are used to navigate relationships between models and perform field lookups, as explained in the previous message.
+from .models import Room, Topic
 
 
 def home(request):
-    # .object blow is the modal manager in django
-    # this below works after quring db because by dafault we have ids genrated for them from 1
 
-    # rooms=Room.objects.all()    #this is how we query the database. we are getting all the objects from the Room model. we are storing it in a variable called rooms. this is a list of objects. we can iterate through this list and get the objects one by one.    
+    q = request.GET.get('query') if request.GET.get('query') != None else ''
 
-    #"result = x if condition else y"    this is the ternary condition in python
-    q=request.GET.get('query') if request.GET.get('query')!=None  else '' #ye "query" home.html mein hai..wwhen clicked on the href link it will send the query to the url and then we will get the query here....we are getting the query from the url and storing it in a variable called q 
-    # now sometime we want to add the multiple parameter below..so to use "and" and "or" we need to import Q from django.db.models see above i have imported it. then i have to wrap the below in Q() and use the | and & operator..simple
-    # rooms=Room.objects.filter(topic__name__icontains=q)
-    
-    
-    # ismein apan ne room modal mein topic ko liye..then topic mein query upward in hte parent kiye..i.w name in topic model then checked whether it contains the query or not.
-    rooms=Room.objects.filter(Q(topic__name__icontains=q) | Q (name__icontains=q)|Q(description__icontains=q))
-    
-    # in python we can get length of list by wrapping it inside len() .function  but .count(), is used to get the length of the query ...this is ,much faster
-    room_count=rooms.count()    #this will count the number of rooms in the database 
-    
-    #this is how we filter the object data from the database. "topic__name__icontains"  filters the ojects which contains "q" in them  ..in this  "i" denotes case-sensitive ness...if i removed the i ..means it is case insensitive
-    # 
-    topics=Topic.objects.all()  
-    
-    #    yaha se apan ne context dict home.html mein bhejo hain ...jaha home.html mein apan ne loops ki help se waha rendere kiya hain
-    context={'rooms':rooms,'topics':topics,'room_count':room_count} #this is a context dictionary....now here the rooms will be from the database and not from the above rooms list.
-    return render(request,'base/home.html',context)             #base/home.html is the path to the home.html file. we dont need to add the template folder as django knows that it is in the template folder automatically. 
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(
+        name__icontains=q) | Q(description__icontains=q))
+
+    room_count = rooms.count()
+
+    topics = Topic.objects.all()
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    return render(request, 'base/home.html', context)
 
 
-# Primary Keys
-# Every table should have a primary key, so every model should have a primary key field. However, you do not have to do this manually if you do not want. By default, Django adds an id field to each model, which is used as the primary key for that model. You can create your own primary key field by adding the keyword arg primary_key=True to a field. If you add your own primary key field, the automatic one will not be added.
+def room(request, pk):
+    room = Room.objects.get(id=pk)
 
-# class Garden(models.Model):
-#     garden_id = models.IntegerField(primary_key=True)
-
-
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
 
 
-
-
-def room(request,pk):           #pk parameter is for dynamic routing
-    room =Room.objects.get(id=pk)        #now we need to get this by unique value ..becaule if we have two value with same value like name,it gonna throw an erroe
-
-    context={'room':room}
-    return render(request,'base/room.html',context) #now since this file is in the base folder in template, we need to add base/room.html. we dont need to add the template folder as django knows that it is in the template folder automatically.
-
-
-
-
-
-
-
-
-# now i wanna restrict the user from accessing some pages based on the status of where the user is logged or logged out. for this we use mixins for classbased views and decorators for function based views. also middleware is used for this purpose
-
-# now i am gonna restrict the create page
-
-# @login_required() #this is a decorator..we just have to add this..but it will not work..we need to add the login_url parameter in it or this will throw an error "page not found 404"
-
-@login_required(login_url='login') #this is a decorator. this will check if the user is logged in or not. if the user is not logged in then it will redirect the user to the login page. we need to import login_required from django.contrib.auth.decorators 
-def createRoom(request):
-    form=RoomForm()     #yaha pe form laya hai form.py se
-    if request.method=='POST':
-        # print(request.POST) #this will print the data that we have submitted in the form in terminal
-
-        # passing all the post data into the form
-        form=RoomForm(request.POST) #this means that we are passing the data that we have submitted in the form to the RoomForm() class in forms.py. the RoomForm() class will validate the data and return it to us. the form knows what fields to expect because we have defined it in the RoomForm() class in forms.py.
-        if form.is_valid(): #this will check if the form is valid or not
-            form.save() #this will save the form in the database 
-            return redirect('home') #this wcccill redirect us to the home page...we need to import redirect from django.shortcuts and in parenthesis i have access the url in this case 'home' by its name in urls.py
-
-
-            # now afer submitting the form we will be redirected to the home page. and we will see the updated rooms there
-
-    return render(request,'base/room_form.html',context={'form':form}) #yaha pe content se form pass kiya hai directly in room_form.html 
-
-
-
-
-# NOw we will do the crud operation ..for this lets create the views
-
-
-# we need to pass the request and pk parameter to the updateRoom function because we are gonna be updating the room with the id=pk
 @login_required(login_url='login')
-def updateRoom(request,pk):
-    room=Room.objects.get(id=pk)        #now we need to get this by unique value ..becaule if we have two value with same value like name,it gonna throw an erroe
-    form=RoomForm(instance=room)        #here we are passing the instance of the room to the RoomForm() class in forms.py. the RoomForm() class will validate the data and return it to us. the form knows what fields to expect because we have defined it in the RoomForm() class in forms.py.
-    
-    
-    # now in theoryi can login and can update the anyone's room ...i want to restrict that user thenso ill do this below....Also this belowline means that if the lggedin user is not the host ofthe room...then it cannot update its room and it will giver a message as u are not allowed
-    if request.user!=room.host:     #this will check if the user is not the host of the room then it will print the below message as a http response
-        return HttpResponse('You are not allowed here')
-    
-    
-    
-    if(request.method=='POST'):
-         form=RoomForm(request.POST,instance=room)   #here we are passing the request.post data which wuill replace the prefill form with data instance=room(wahtever that is in the database)
-         if form.is_valid():
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+
+        form = RoomForm(request.POST)
+        if form.is_valid():
             form.save()
             return redirect('home')
-    context={'form':form}
-    return render(request,'base/room_form.html',context) #yaha pe content se form pass kiya hai directly in room_form.html
 
+    return render(request, 'base/room_form.html', context={'form': form})
 
-# this below methoid is for deleting the room directly from the home page
-# created by me
-
-# def deleteRoom(request,pk):
-#     room=Room.objects.get(id=pk)
-#     # if(request.method=='POST'):
-#     room.delete() 
-#     return redirect('home')
-#     context={'object':room}
-#     return render(request,'base/delete.html',context) #yaha pe content se form pass kiya hai directly in room_form.html
 
 @login_required(login_url='login')
-def deleteRoom(request,pk):
-    room=Room.objects.get(id=pk)
-    form=RoomForm(request.POST)
-    if(request.method=='POST'):
-        room.delete()       #this will delete the room from the database
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here')
+
+    if (request.method == 'POST'):
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+@login_required(login_url='login')
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(request.POST)
+    if (request.method == 'POST'):
+        room.delete()
         return redirect('home')
-    
-    return render(request,'base/delete.html',context={'obj':room})
-    
-    # in this below way also can we return the form
-    # return render(request=request,template_name='base/delete.html',context={'obj':room})
 
-#dont use login as a function name as it is a inbuilt function in django which we will be using
+    return render(request, 'base/delete.html', context={'obj': room})
+
+
 def loginPage(request):
-    # we have added this page for a reason...to apply the cond in the login_register.html file..that if the page is login the show login...if the page is register then show register
-    page='login'
-    # now if the user is alreadt login he can still access the page by going to the url..so we need to restrict that.jaise hi user login page ("/login") mein jayega ye wala view trigger hoga  then this view niche noche check karega ki kya user already login hain..agar hain toh woh directly use redirect karega and if nahi login hain toh use login page pe jane dega...... see below
-    if request.user.is_authenticated:   #this will check if the user is authenticated or not
-        return redirect('home')         #if the user is authenticated then it will redirect the user to the home page
-    
-    if request.method=='POST':
-        userid=request.POST.get('username').lower().lower()
-        password=request.POST.get('password')
+
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        userid = request.POST.get('username').lower().lower()
+        password = request.POST.get('password')
         try:
-            # explainnation of the below line
-            
-#             User.objects is a manager for the User model. It provides an interface for querying the database and performing operations on the User model.
 
-# .get() is a method provided by the manager. It's used for retrieving a single object from the database that matches the specified conditions.
+            user = User.objects.get(username=userid)
+        except:
+            messages.error(request, 'User does not exist')
 
-# username=userid is specifying the condition for the query. It's saying, "Retrieve a user object where the username field is equal to the value stored in the variable userid."
-
-# So, in essence, this line is trying to retrieve a user from the database whose username matches the one provided in the userid variable. If such a user exists, it will be stored in the variable user. If not, it will raise an exception (which you've handled with a try and except block).
-            user=User.objects.get(username=userid)
-        except:     # if te user does not exist below block  ..this is how we handle the error in django
-            messages.error(request,'User does not exist')        #this is how i show message in django...we have writeen its statements in the main.html go see that
-        #if the user does exist
-        
-        # used the authenticate function(inbuilt fun) to authenticate the user
-        
-        # this below authenticate function will return the user Object if the user is authenticated else it will return None
-        user=authenticate(request,username=userid,password=password) #this will authenticate the user
+        user = authenticate(request, username=userid, password=password)
         if user is not None:
-            login(request,user) #login function is inbuilt in django, it will login the user and create a session for the user in the database and inside of our browser..then the suer will be officially logged in
+            login(request, user)
             return redirect('home')
         else:
-            messages.error(request,'Username or password is incorrect')
-    context={'page':page}
-    return render(request,'base/login_register.html',context)
-
-
-
-
-# here i have directly logoiut the user from the home page and havent created any logout template
-
-
-# In Django, the logout function requires the request object in order to log out the user associated with that specific request. The request object contains information about the current web request, including details about the user making the request. When you call logout(request), Django uses the information from the request object to identify which user is currently authenticated and logs them out.
-
-# In summary, the request object is necessary for the logout function to perform the logout action on the correct user.
+            messages.error(request, 'Username or password is incorrect')
+    context = {'page': page}
+    return render(request, 'base/login_register.html', context)
 
 
 def logoutUser(request):
-    logout(request)     #this willd delete the session of the user from the database and from the browser
+    logout(request)
     return redirect('home')
 
 
 def registerPage(request):
-    if request.method=='POST':
-        form=UserCreationForm(request.POST)
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            user=form.save(commit=False)    #this will save the form but will not commit it to the database 
-            # now i want to login the user after he has registered
-            user.username=user.username.lower()    #this will convert the username to lowercase
+            user = form.save(commit=False)
+
+            user.username = user.username.lower()
             user.save()
-            login(request,user)     #here i have logged ther user in..the i have redireted to home page
+            login(request, user)
             return redirect('home')
         else:
-            messages.error(request,'An error occured during the  registration')
+            messages.error(
+                request, 'An error occured during the  registration')
 
-    form=UserCreationForm()
-    context={'form':form}
-    return render(request,'base/login_register.html',context)
+    form = UserCreationForm()
+    context = {'form': form}
+    return render(request, 'base/login_register.html', context)
