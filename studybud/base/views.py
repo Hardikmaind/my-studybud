39e40ebbb07fff0c45765b707157a3760a89673c
Hardikmaind <maindhardik@gmail.com>
2023-10-07 00:14:16 +0530

    done room createion and updation form pge

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -94,11 +94,17 @@ def updateRoom(request, pk):
         return HttpResponse('You are not allowed here')
 
     if (request.method == 'POST'):
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    context = {'form': form,'topics':topics}
+        topic_name=request.POST.get('topic')
+        topic,created=Topic.objects.get_or_create(name=topic_name)
+        room.name=request.POST.get('name')
+        room.topic=topic
+        room.description=request.POST.get('description')
+        room.save()
+        # form = RoomForm(request.POST, instance=room)
+        # if form.is_valid():
+        #     form.save()
+        return redirect('home')
+    context = {'form': form,'topics':topics,'room':room}
     return render(request, 'base/room_form.html', context)
 
 
