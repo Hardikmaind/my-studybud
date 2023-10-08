bc074dd999eddd62743c4913ee95553b3c58c0b3
Hardikmaind <maindhardik@gmail.com>
2023-10-08 16:34:31 +0530

    made an api with the help of DRF and also used the serializer

--- studybud/base/api/views.py
+++ studybud/base/api/views.py
@@ -1,5 +1,12 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from  base.models import Room
+from .serializers import RoomSerializer
 
+# niche aopan ko api view mein req allow akrni hain apan woh dal sakte hain...abhi k liye sirf main 'GET' hi dalta hu
+# @api_view(['GET','PUT','POSt'])
+
+@api_view(['GET'])
 def getRoutes(request):
     # below routes is a list
     routes=[
@@ -8,5 +15,23 @@ def getRoutes(request):
         'GET /api/rooms/:id',
     ]
     
-    # this below JsonResponse will return routes in the json format it will convert it into the json format...also sage=False neans..this will convert the above python list into json..means it is not strict
-    return JsonResponse(routes,safe=False)
+    # niche apan ko safe vagare ki bhi koi jarurat nahi
+    # return Response(routes)
+    return Response(routes)
+
+
+# now this below will give the error that ("Object of type Room is not JSON serializable") since first we were returning the reoutes which is dictionry and can be converted into the json..but now below thge room is coming from the Room(class/model/table) we first have to serialize it. serialize means (converting from class to json).
+# from this is it also clear that the Response cannot returned the pyuthon objects(rooms). Objects cannot be converted automaticlly
+
+# below i have only allowed the get request
+@api_view(['GET'])
+def getRooms(request):
+    rooms=Room.objects.all()
+    # here below many means that are there gonna be the multipe objects that we need to serialize or are we just serialiuzing one...in this case we are serializeing the query set. so many is gonna be set True, there are many objects
+    serializer=RoomSerializer(rooms,many=True)
+    # now after this the above serializer variable is now an object so in this case we will directly pass it in the reponse
+    
+    # return Response(serializer)
+    
+    # now since we dont want to return the object ...we want to return a data attribute inside it..so we will do it like below
+    return Response(serializer.data)
--- studybud/studybud/urls.py
+++ studybud/studybud/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),    #so when every url matches empty string, it will go to the base.urls file . this will take us to the home page. and let base.urls handle the rest of the routing...this file is present in the base folder and it is the urls.py file that we are taretting
     
-    path('api/',include('base.api.urls'))
+    path('api/',include('base.api.urls')),
 ]
