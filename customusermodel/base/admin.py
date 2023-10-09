258e9c273d4b42f828e47225dd9e9db03aca07d4
Hardikmaind <maindhardik@gmail.com>
2023-10-09 23:32:15 +0530

    added the user model inside the admin panel ..

--- customusermodel/base/admin.py
+++ customusermodel/base/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+# now typically we dont have to register our user with the admin panel but because we created our own user model we do need to go into the admin.py..and do this..
+
+
+# here we have imported the User model which we have extendewd form the AbstractUser model which is inbuilt in python 
+
+# isse apan ne jo bhi users banaye woh saare aajayenge..in the admin panl
+# also notice that the  User groop insnt under the authenticaiton and authorization panel...but under the Base which we have created
+admin.site.register(User)
--- customusermodel/base/models.py
+++ customusermodel/base/models.py
@@ -1,12 +1,13 @@
 # Import the necessary modules from Django
-from django.db import models  # Import the models module for defining database models
+from django.db import models  
 from django.contrib.auth.models import AbstractUser  # Import the AbstractUser class for custom user model
 
 
-#  a Django model class named User that inherits from AbstractUser. This code is a common way to extend Django's built-in authentication system to create a custom user model with additional fields or functionality.
 
 
 
 # Define a custom user model that inherits from AbstractUser
 class User(AbstractUser):
-    pass  # "pass" is a placeholder and indicates that no additional custom fields or methods are added
+    pass  
+
+    # now typically we dont have to register our user with the admin panel but because we created our own user model we do need to go into the admin.py and...follow further their
