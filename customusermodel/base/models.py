623eb6ca61c55b998ada1ce5f0bf4b206e453166
Hardikmaind <maindhardik@gmail.com>
2023-10-09 23:41:12 +0530

    added the email bio and name field in the user model..now lets do chages in the studybud. done work with the customusermodel

--- customusermodel/base/models.py
+++ customusermodel/base/models.py
@@ -8,6 +8,15 @@ from django.contrib.auth.models import AbstractUser  # Import the AbstractUser c
 
 # Define a custom user model that inherits from AbstractUser
 class User(AbstractUser):
-    pass  
+    # u can see the name bio and email field int the admin panel
+    name=models.CharField(max_length=200,null=True)
+    email=models.EmailField(unique=True)
+    bio=models.TextField(null=True)
+    
+    # now we will be saying the django that the username and the password field
+    USERNAME_FIELD='email'      #this will be the username field which will be used for the login
+    REQUIRED_FIELDS=[]          #this will be required field which will be compulsory
+    
+    # after creating the aboce make migrations
 
     # now typically we dont have to register our user with the admin panel but because we created our own user model we do need to go into the admin.py and...follow further their
